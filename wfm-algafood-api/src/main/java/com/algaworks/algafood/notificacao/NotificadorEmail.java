48f72f0a03fc5a1cce0c742e7c3bdc16aecc154d
William Felipe Marx <marxwilliamf@gmail.com>
2023-01-14 16:58:03 -0300

    Aula 2.18 - Desambiguação de beans com @Qualifier

--- wfm-algafood-api/src/main/java/com/algaworks/algafood/notificacao/NotificadorEmail.java
+++ wfm-algafood-api/src/main/java/com/algaworks/algafood/notificacao/NotificadorEmail.java
@@ -1,11 +1,12 @@
 package com.algaworks.algafood.notificacao;
 
-import org.springframework.context.annotation.Primary;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 import com.algaworks.algafood.modelo.Cliente;
 
-@Primary //faz a desambiguação de beans //quando tem mais de um para ser usado em uma mesma injeção o que tiver com o primary é que va ser usado
+//@Primary //faz a desambiguação de beans //quando tem mais de um para ser usado em uma mesma injeção o que tiver com o primary é que va ser usado
+@Qualifier("normal") //("email") //poderia colocar email mas deixaria o sistema mais acoplado, em casos de mudanças
 @Component
 public class NotificadorEmail implements Notificador { //implements Notificador
 	
--- wfm-algafood-api/src/main/java/com/algaworks/algafood/notificacao/NotificadorSMS.java
+++ wfm-algafood-api/src/main/java/com/algaworks/algafood/notificacao/NotificadorSMS.java
@@ -1,9 +1,11 @@
 package com.algaworks.algafood.notificacao;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 import com.algaworks.algafood.modelo.Cliente;
 
+@Qualifier("urgente") //("sms") poderia colocar sms mas deixaria o sistema mais acoplado, em casos de mudanças
 @Component
 public class NotificadorSMS implements Notificador { //implements Notificador
 		
