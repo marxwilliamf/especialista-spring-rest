849dec14b2abeade69531d22ef368cdd6a98c1b4
William Felipe Marx <marxwilliamf@gmail.com>
2023-01-15 15:31:57 -0300

    Aula 2.21 Parte III: implementando interfaces InitializingBean, DisposableBean - Criando métodos de callback do ciclo de vida dos beans

--- wfm-algafood-api/src/main/java/com/algaworks/algafood/service/AtivacaoClienteService.java
+++ wfm-algafood-api/src/main/java/com/algaworks/algafood/service/AtivacaoClienteService.java
@@ -3,6 +3,8 @@ package com.algaworks.algafood.service;
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -12,27 +14,39 @@ import com.algaworks.algafood.modelo.TipoDoNotificador;
 import com.algaworks.algafood.notificacao.Notificador;
 
 
-//@Component //bean //componente gerenciado pelo spring que responde requisições web
-public class AtivacaoClienteService {
+@Component //bean //componente gerenciado pelo spring que responde requisições web
+public class AtivacaoClienteService implements InitializingBean, DisposableBean{
 	
 	@TipoDoNotificador(NivelUrgencia.SEM_URGENCIA)
 	@Autowired(required = false)
 	private Notificador notificador; //Recebe uma lista de notificadores
 	
-	//@PostConstruct  //callback
-	public void init () {
-		System.out.println("INIT " + notificador);
-	}
-	
-	//@PreDestroy  //callback
-	public void destroy () {
-		System.out.println("DESTROY");
-	}
+//	//@PostConstruct  //callback
+//	public void init () {
+//		System.out.println("INIT " + notificador);
+//	}
+//	
+//	//@PreDestroy  //callback
+//	public void destroy () {
+//		System.out.println("DESTROY");
+//	}
 	
 	public void ativar(Cliente cliente) {
 	
 		cliente.ativar();
 		notificador.notificar(cliente, "Seu cadastro no sistema está ativo!");
+	}
+
+	@Override
+	public void destroy() throws Exception {
+		System.out.println("DESTROY");
+		
+	}
+
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		System.out.println("INIT " + notificador);
+		
 	}	
 }
 
