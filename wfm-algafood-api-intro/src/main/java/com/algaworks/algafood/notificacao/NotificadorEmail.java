2fd00a61f62d562ccfb560ed52aed0aa178bb66d
William Felipe Marx <marxwilliamf@gmail.com>
2023-01-16 19:41:47 -0300

    Renomeando projeto para '-intro', pois começaremos um novo projeto com o nome antigo

--- wfm-algafood-api-intro/src/main/java/com/algaworks/algafood/notificacao/NotificadorEmail.java
+++ wfm-algafood-api-intro/src/main/java/com/algaworks/algafood/notificacao/NotificadorEmail.java
@@ -1,7 +1,6 @@
 package com.algaworks.algafood.notificacao;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
