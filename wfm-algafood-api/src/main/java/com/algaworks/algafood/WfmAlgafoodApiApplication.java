e70a758d08310e894c3af4574e5b89d19f46ddb6
William Felipe Marx <marxwilliamf@gmail.com>
2023-01-16 21:54:42 -0300

    Aula 3.3 - Adicionando JPA e configurando o Data Source

---
+++ wfm-algafood-api/src/main/java/com/algaworks/algafood/WfmAlgafoodApiApplication.java
@@ -0,0 +1,13 @@
+package com.algaworks.algafood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WfmAlgafoodApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WfmAlgafoodApiApplication.class, args);
+	}
+
+}
