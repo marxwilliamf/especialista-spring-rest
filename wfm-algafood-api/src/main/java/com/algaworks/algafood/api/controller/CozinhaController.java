dd1e37d51c590c39b9876ea95778a908766b666d
William Felipe Marx <marxwilliamf@gmail.com>
2023-07-11 18:55:37 -0300

    Aula 4.20 parte II - Manipulando a resposta HTTP com ResponseEntity

--- wfm-algafood-api/src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ wfm-algafood-api/src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -3,6 +3,7 @@ package com.algaworks.algafood.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -58,7 +59,16 @@ public class CozinhaController {
 		
 		//return ResponseEntity.status(HttpStatus.OK).build(); //caso não tenha um corpo para retornar
 		//return ResponseEntity.status(HttpStatus.OK).body(cozinha);//
-		 return ResponseEntity.ok(cozinha); //shortcut
+		//return ResponseEntity.ok(cozinha); //shortcut
+		 
+		 
+		 //Exemplo redirecionamento
+		 HttpHeaders headers = new HttpHeaders();
+		 headers.add(HttpHeaders.LOCATION, "http://api.algafood.local:8080/cozinhas"); //o recuros redirecionado é outro mas é só para mostrar um redirecionamento
+		 
+		 return ResponseEntity.status(HttpStatus.FOUND) //endereço encontrado mas movido temporáriamente //para ver o status desativar o redirect do Postman
+				 .headers(headers)
+				 .build();
 	}
 	
 	
