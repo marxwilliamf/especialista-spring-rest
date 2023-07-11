f9566b18b1dc56a282c70e7d55cbefce75c74fb5
William Felipe Marx <marxwilliamf@gmail.com>
2023-07-11 18:16:50 -0300

    Aula 4.19 - Definindo o status da resposta HTTP com @ResponseStatus

--- wfm-algafood-api/src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ wfm-algafood-api/src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -3,11 +3,13 @@ package com.algaworks.algafood.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 //import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 //import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -29,24 +31,27 @@ import com.algaworks.algafood.domain.repository.CozinhaRepository;
 ///////////////////////////////////
 
 @RestController
-@RequestMapping("/cozinhas")//em caso de mais de um parametro tem que especificar value (value = "/cozinhas", produces = MediaType.APPLICATION_JSON_VALUE) //é possivel fazer uma definição para toda classe também
+@RequestMapping("/cozinhas")
 public class CozinhaController {
 
 
 	@Autowired
 	CozinhaRepository cozinhaRepository;
 	
-	//@GetMapping(produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE }) //este método aceitará os dois desse jeito
+	
+
 	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)  //tanto faz usar a constante ou escrever, mas é mais recomandável constante //(produces = "application/json")
 	public List<Cozinha> listar1() {
 		return cozinhaRepository.listar();
 	}
 	
+	
 	@GetMapping(produces = MediaType.APPLICATION_XML_VALUE)
 	public CozinhasXmlWrapper listar2() {
 		return new CozinhasXmlWrapper(cozinhaRepository.listar());
 	}
-	
+
+	@ResponseStatus(HttpStatus.CREATED) //caso queira modificar o código de status, porém 201 não é o correto para essa requisição
 	@GetMapping("/{cozinhaId}")
 	public Cozinha buscar(@PathVariable Long cozinhaId) { // = //(@PathVariable("cozinhaId") Long id) { 
 		return cozinhaRepository.buscar(cozinhaId);
