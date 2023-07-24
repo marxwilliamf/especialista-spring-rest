18bd412af6eb9b2b0000c2b1caae9ff561dec592
William Felipe Marx <marxwilliamf@gmail.com>
2023-07-24 15:09:06 -0300

    Aula 4.23 - Modelando e implementando a inclusão de recursos com POST

--- wfm-algafood-api/src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ wfm-algafood-api/src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -9,8 +9,11 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 //import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 //import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -66,5 +69,17 @@ public class CozinhaController {
 
 	}
 	
+	////////////////// se não quizesse retornar nada
+//	@PostMapping
+//	@ResponseStatus(HttpStatus.CREATED)
+//	public void adiciona(@RequestBody Cozinha cozinha) {
+//		cozinhaRepository.adicionar(cozinha);
+//	}
+///////////////////////////////
 	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Cozinha adiciona(@RequestBody Cozinha cozinha) {
+		return cozinhaRepository.adicionar(cozinha);
+	}
 }
